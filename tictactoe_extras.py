f0823c6eb883ab5691a590fb91e2ed8ca7aa392c
jasnatbyui <jasnatbyui@gmail.com>
2022-01-12 22:27:25 +1300

    def changes has_winner

--- tictactoe_extras.py
+++ tictactoe_extras.py
@@ -37,14 +37,18 @@ def is_a_draw(board):
     return True 
     
 def has_winner(board):
-    return (board[0] == board[1] == board[2] or
-            board[3] == board[4] == board[5] or
-            board[6] == board[7] == board[8] or
-            board[0] == board[3] == board[6] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[0] == board[4] == board[8] or
-            board[2] == board[4] == board[6])
+    return (board[0] == board[1] == board[2] == board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] == board[9] == board[10] == board[11] or
+            board[2] == board[13] == board[14] == board[15] == board[16] == board[17] or
+            board[18] == board[19] == board[20] == board[21] == board[22] == board[23] or
+            board[24] == board[25] == board[26] == board[27] == board[28] == board[29] or
+            board[30] == board[31] == board[32] == board[33] == board[34] == board[35] or
+            board[0] == board[6] == board[12] == board[18] == board[24] == board[30] or
+            board[1] == board[7] == board[13] == board[19] == board[25] == board[31] or
+            board[2] == board[8] == board[14] == board[20] == board[26] == board[32] or
+            board[3] == board[9] == board[15] == board[21] == board[27] == board[33]or
+            board[4] == board[10] == board[16] == board[22] == board[28] == board[43]or
+            board[5] == board[11] == board[17] == board[23] == board[29] == board[35])
 
 def make_move(player, board):
     square = int(input(f"{player}'s turn to choose a square (1-9): "))
